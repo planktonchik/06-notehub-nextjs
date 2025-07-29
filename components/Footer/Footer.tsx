0abc0da559ae502632a7068f64aab702de9b5bd6
Plankton <planktonchik88@gmail.com>
2025-07-29 18:06:28 +0300

    add footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: TimKaRo</p>
+          <p>Developer: Anna</p>
           <p>
             Contact us:
             <a href="mailto:student@notehub.app">student@notehub.app</a>
